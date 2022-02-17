3050f191e16fee75be7347afe13163c759f0722e
phamtanphat <phatdroid94@gmail.com>
2022-02-17 19:33:35 +0700

    finish

--- app/src/main/java/com/example/thread30112021/MainActivity.java
+++ app/src/main/java/com/example/thread30112021/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.thread30112021;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int a , b , c;
     MyFlag myFlag;
+    Handler handler;
+    TextView mTvA,mTvB,mTvC;
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvA = findViewById(R.id.textA);
+        mTvB = findViewById(R.id.textB);
+        mTvC = findViewById(R.id.textC);
 
         a = b = c = 0;
         myFlag = new MyFlag(0);
@@ -22,6 +33,23 @@ public class MainActivity extends AppCompatActivity {
         // 0 => in ra a
         // 1 => in ra b
         // 2 => in ra c
+        handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case 0 :
+                        mTvA.setText("A " + msg.obj);
+                        break;
+                    case 1 :
+                        mTvB.setText("B " + msg.obj);
+                        break;
+                    case 2 :
+                        mTvC.setText("C " + msg.obj);
+                        break;
+                }
+            }
+        };
 
         Thread threadA = new Thread(new Runnable() {
             @Override
@@ -30,7 +58,15 @@ public class MainActivity extends AppCompatActivity {
                    for (int i = 0; i < 10 ;) {
                       if (myFlag.count == 0){
                           a = i;
-                          Log.d("BBB","A = " + a);
+                          try {
+                              Thread.sleep(200);
+                          } catch (InterruptedException e) {
+                              e.printStackTrace();
+                          }
+                          Message message = new Message();
+                          message.what = 0;
+                          message.obj = a;
+                          handler.sendMessage(message);
                           myFlag.count = 1;
                           myFlag.notifyAll();
                           i++;
@@ -53,7 +89,15 @@ public class MainActivity extends AppCompatActivity {
                     for (int i = 0; i < 10 ; ) {
                         if (myFlag.count == 1){
                             b = i;
-                            Log.d("BBB","B = " + b);
+                            try {
+                                Thread.sleep(200);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            Message message = new Message();
+                            message.what = 1;
+                            message.obj = b;
+                            handler.sendMessage(message);
                             myFlag.count = 2;
                             myFlag.notifyAll();
                             i++;
@@ -76,7 +120,15 @@ public class MainActivity extends AppCompatActivity {
                    for (int i = 0; i < 10 ;) {
                       if (myFlag.count == 2){
                           c = a + b;
-                          Log.d("BBB","C = " + c);
+                          try {
+                              Thread.sleep(200);
+                          } catch (InterruptedException e) {
+                              e.printStackTrace();
+                          }
+                          Message message = new Message();
+                          message.what = 2;
+                          message.obj = c;
+                          handler.sendMessage(message);
                           myFlag.count = 0;
                           myFlag.notifyAll();
                           i++;
