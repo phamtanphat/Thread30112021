62e5aece3efea91dfc4e256c3641fe5da901a0ff
phamtanphat <phatdroid94@gmail.com>
2022-02-17 19:06:53 +0700

    udate

--- app/src/main/java/com/example/thread30112021/MainActivity.java
+++ app/src/main/java/com/example/thread30112021/MainActivity.java
@@ -8,43 +8,93 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a , b , c;
+    MyFlag myFlag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        a = b = c = 0;
+        myFlag = new MyFlag(0);
+
+        // count
+        // 0 => in ra a
+        // 1 => in ra b
+        // 2 => in ra c
+
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                printLog("A");
+               synchronized (myFlag){
+                   for (int i = 0; i < 10 ;) {
+                      if (myFlag.count == 0){
+                          a = i;
+                          Log.d("BBB","A = " + a);
+                          myFlag.count = 1;
+                          myFlag.notifyAll();
+                          i++;
+                      }else {
+                          try {
+                              myFlag.wait();
+                          } catch (InterruptedException e) {
+                              e.printStackTrace();
+                          }
+                      }
+                   }
+               }
             }
         });
 
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                printLog("B");
+                synchronized (myFlag){
+                    for (int i = 0; i < 10 ; ) {
+                        if (myFlag.count == 1){
+                            b = i;
+                            Log.d("BBB","B = " + b);
+                            myFlag.count = 2;
+                            myFlag.notifyAll();
+                            i++;
+                        }else {
+                            try {
+                                myFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
             }
         });
-        threadA.start();
-        threadB.start();
 
-        new Handler().postDelayed(new Runnable() {
+        Thread threadC = new Thread(new Runnable() {
             @Override
             public void run() {
-                Log.d("CCC",threadA.getState().toString());
-                Log.d("CCC",threadB.getState().toString());
+               synchronized (myFlag){
+                   for (int i = 0; i < 10 ;) {
+                      if (myFlag.count == 2){
+                          c = a + b;
+                          Log.d("BBB","C = " + c);
+                          myFlag.count = 0;
+                          myFlag.notifyAll();
+                          i++;
+                      }else {
+                          try {
+                              myFlag.wait();
+                          } catch (InterruptedException e) {
+                              e.printStackTrace();
+                          }
+                      }
+                   }
+               }
             }
-        },2000);
-    }
-    private synchronized void printLog(String text){
-        for (int i = 0; i < 100 ; i++) {
-            Log.d("BBB",text + " : " + i);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
     }
 }
---
+++ app/src/main/java/com/example/thread30112021/MyFlag.java
@@ -0,0 +1,9 @@
+package com.example.thread30112021;
+
+public class MyFlag {
+    int count;
+
+    public MyFlag(int count){
+        this.count = count;
+    }
+}
